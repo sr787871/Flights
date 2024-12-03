c5516cfe9f9d5a4c0f3431363ee2e32faf32f33e
Sachin Rawat <sr787871@gmail.com>
2024-12-03 17:27:31 +0530

    Clean the Error handling

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,6 +1,7 @@
 const { response } = require("express");
 const {AirplaneService} = require("../services")
 const {StatusCodes} = require("http-status-codes")
+const {SuccessResponse , ErrorResponse} = require("../utils/common")
 
 // Post : /airplane
 // reqbody {modelNumber:'airbus320', capacity:200}
@@ -12,19 +13,11 @@ async function createAirplane(req,res){
             modelNumber:req.body.modelNumber,
             capacity: req.body.capacity
         });
-        return res.status(StatusCodes.CREATED).json({
-            success : true,
-            message : "Successfully created an airplane",
-            data : airplane,
-            error : {},
-        })        
+        SuccessResponse.data = airplane
+        return res.status(StatusCodes.CREATED).json({SuccessResponse})        
     } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            success : false,
-            message : "Something went wrong while creating airplane",
-            data : {},
-            error : error,
-        });
+        ErrorResponse.error = error
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ErrorResponse});
     }
 }
 
---
+++ src/utils/common/error-response.js
@@ -0,0 +1,8 @@
+const error = {
+    success : false,
+    message : "Something went wrong",
+    data : {},
+    message : {},
+}
+
+module.exports = error;
