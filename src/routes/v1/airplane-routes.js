39e3c23ef4bc8c852f93cadb54014204b88afa6a
Sachin Rawat <sr787871@gmail.com>
2024-12-03 17:01:02 +0530

    Added middleware for Validations

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const {AirplaneController} = require("../../controllers")
+const {AirplaneController} = require("../../controllers");
+const {AirplaneMiddlewares } = require("../../middlewares");
 const airplaneRoutes = express.Router();
 
 // api/v1/airplanes POST
-airplaneRoutes.post('/',AirplaneController.createAirplane);
+airplaneRoutes.post('/',
+    AirplaneMiddlewares.validateCreateRequest,  // Adding middlewares for validation
+    AirplaneController.createAirplane
+);
 
 module.exports = airplaneRoutes;
