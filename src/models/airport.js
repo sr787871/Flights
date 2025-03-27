c4b6150a0291852d4907befc2ed3270ebcd76766
Sachin Rawat <sr787871@gmail.com>
2025-03-27 16:10:05 +0530

    Added flights model with foreign key constraint defined during model definition ```     npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departueAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departueTime:date,boardingGate:string,totalSeats:integer ```

--- src/models/airport.js
+++ src/models/airport.js
@@ -15,7 +15,16 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey : 'cityId',
         onDelete : 'CASCADE',
         // onUpdate : 'CASCADE'
-      })
+      });
+      this.hasMany(models.Flight,{
+        foreignKey : 'departueAirportId',
+        onDelete : 'CASCADE',
+      });
+      this.hasMany(models.Flight,{
+        foreignKey : 'arrivalAirportId',
+        onDelete : 'CASCADE',
+      });
+
     }
   }
   Airport.init({
---
+++ src/models/flight.js
@@ -0,0 +1,62 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flight extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Airplane,{
+        foreignKey : 'airplaneId'
+      });
+      this.belongsTo(models.Airport,{
+        foreignKey : 'departueAirportId'
+      });
+      this.belongsTo(models.Airport,{
+        foreignKey : 'arrivalAirportId'
+      })
+    }
+  }
+  Flight.init({
+    flightNumber: {
+      type : DataTypes.STRING,
+      allowNull : false,
+    },
+    airplaneId: {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+    },
+    departueAirportId: {
+      type : DataTypes.STRING,
+      allowNull : false,
+    },
+    arrivalAirportId: {
+      type : DataTypes.STRING,
+      allowNull : false,
+    },
+    arrivalTime:{
+      type :  DataTypes.DATE,
+      allowNull : false,
+    },
+    departueTime: {
+      type : DataTypes.DATE,
+      allowNull : false,
+    },
+    boardingGate: {
+      type : DataTypes.STRING,
+    },
+    totalSeats: {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Flight',
+  });
+  return Flight;
+};
