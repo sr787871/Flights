fdee3d54f0ffb5d7ec8c464d4097e832cbc2d0ac
Sachin Rawat <sr787871@gmail.com>
2025-03-29 14:10:01 +0530

    Fix:Price is added in flight-controller

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -14,6 +14,7 @@ async function createFlight(req,res){
             arrivalAirportId : req.body.arrivalAirportId,
             arrivalTime : req.body.arrivalTime,
             departueTime : req.body.departueTime,
+            price : req.body.price,
             boardingGate : req.body.boardingGate,
             totalSeats : req.body.totalSeats,
         });
