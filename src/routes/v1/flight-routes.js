c10f6dae4e36cebb08f519111f2de60c2cd2bb3a
Sachin Rawat <sr787871@gmail.com>
2025-04-05 14:23:28 +0530

    Added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -3,16 +3,22 @@ const {FlightController} = require("../../controllers");
 const {FlightMiddlewares} = require("../../middlewares");
 const flightRoutes = express.Router();
 
-// api/v1/airplanes POST
+// api/v1/flights POST
 flightRoutes.post('/',
     FlightMiddlewares.validateCreateRequest,  // Adding middlewares for validation
     FlightController.createFlight
 );
 
-// api/v1/airplanes POST
+// api/v1/flights POST
 flightRoutes.get('/',
     // FlightMiddlewares.validateCreateRequest,  // Adding middlewares for validation
     FlightController.getAllFlights
+);
+
+// api/v1/flights/:id POST
+flightRoutes.get('/:id',
+    // FlightMiddlewares.validateCreateRequest,  // Adding middlewares for validation
+    FlightController.getFlight
 );
 
 module.exports = flightRoutes;
