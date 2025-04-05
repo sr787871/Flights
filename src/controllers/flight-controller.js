e5c1589b9585b0634f8f3823e40b4e286f9faa3f
Sachin Rawat <sr787871@gmail.com>
2025-04-05 16:10:01 +0530

    Added Update Remaining Seats Api and also added row-level locking

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -50,8 +50,26 @@ async function getFlight(req,res){
     }
 }
 
+// GET : /flights/:id/seats
+// req.body : { seats : 10 }
+async function updateSeats(req,res){
+    try {
+        const response = await FlightService.updateRemainingSeats({
+            flightId : req.params.id,
+            seats : req.body.seats,
+            dec : req.body.dec,
+        })
+        SuccessResponse.data = response
+        return res.status(StatusCodes.OK).json(SuccessResponse)
+    } catch (error) {
+        ErrorResponse.error = error
+        return res.status(error.statusCode).json(ErrorResponse);
+    }
+}
+
 module.exports = {
     createFlight,
     getAllFlights,
-    getFlight
+    getFlight,
+    updateSeats
 }
